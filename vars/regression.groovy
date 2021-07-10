88e6b2f0520436e1310c06ba5cf4bd12a2d0e706
ranjitnitc <40832424+ranjitnitc@users.noreply.github.com>
2021-07-10 12:22:39 +0530

    Update regression.groovy

--- vars/regression.groovy
+++ vars/regression.groovy
@@ -1,6 +1,11 @@
 #!/usr/bin/env groovy
 
 def call(body) {
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+  
   def name =config.get('name', false)
   echo "Hey ${name}, How are you??"
 }
