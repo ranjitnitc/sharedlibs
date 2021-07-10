40c4b8a1a1f832cba6bbb1eb7be63d8b77374619
ranjitnitc <40832424+ranjitnitc@users.noreply.github.com>
2021-07-10 14:05:22 +0530

    add approve stage

--- vars/regression.groovy
+++ vars/regression.groovy
@@ -11,7 +11,21 @@ def call(body) {
   echo "Hey ${name}, How are you??"
   echo "i live in ${state}"
   node('master'){
+    stage("Build") {
         sh "cd /tmp; ls -lrt ; date"
         checkout scm
        }  
+    stage("approve deploy"){
+      node('paused'){
+        try{
+          abortPreviousBuilds(stage: "approve deploy" ) 
+          timeout(time: 20 , unit: 'MINUTES' )
+           input message:'approve this build'
+           milestone()   
+        } catch (err) {
+          println err
+        }
+      }
+    }
+  }
 }
